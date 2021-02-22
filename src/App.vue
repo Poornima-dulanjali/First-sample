6c3ffd0609edc3495ea356020514b4e488dcccd4
Poornima <poornima.dulanjalee@gmail.com>
2021-02-22 21:20:31 +0530

    First sample message

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <first msg="First component"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import first from './components/first.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    first
   }
 }
 </script>
---
+++ src/components/first.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="demo">
+    <h1>{{ msg }}</h1>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'first',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.demo {
+  background-color : cyan;
+}
+
+</style>
